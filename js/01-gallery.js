790804a9d220db7be87f017ef65ce3b43f70c470
seacat271 <seacat271083@gmail.com>
2022-08-08 13:32:35 +0300

    add lazyload

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,31 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryContainer = document.querySelector(".gallery");
-console.log(galleryContainer);
+
 
 const createGalleryMarkup = galleryItems.map(({preview, original, description}) => {
-return `<div class="gallery__item"><a class = "gallery__link" href="${original}"><img class ="gallery__image" src="${preview}" data-source="${original}" alt="${description}"></a></div>`
+return `<div class="gallery__item"><a class = "gallery__link" href="${original}"><img class ="gallery__image lazyload" data-src="${preview}" data-source="${original}" alt="${description}" loading="lazy"></a></div>`
 }).join("")
-// console.log(galleryMarkup)
 
 galleryContainer.innerHTML = createGalleryMarkup;
+
+if ("loading" in HTMLImageElement.prototype){
+    const lazyImages = document.querySelectorAll(".gallery__image")
+    console.log(lazyImages)
+    lazyImages.forEach(img => {
+        img.src = img.dataset.src;
+    });
+} else {
+const scriptLazyLib = document.createElement("script");
+scriptLazyLib.src = src="https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+scriptLazyLib.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ=="
+scriptLazyLib.crossOrigin = "anonymous";
+scriptLazyLib.referrerPolicy = "no-referrer";
+}
+
 galleryContainer.addEventListener("click", onClickGalleryItem)
-window.addEventListener("keydown", onPressToEscape)
 
 function onClickGalleryItem (event) {
         event.preventDefault()
         if (event.target.nodeName !== "IMG") {return}
         const modalWindow = basicLightbox.create(`<img src=${event.target.dataset.source}>`)
         modalWindow.show();
-        console.log(modalWindow.visible())  
-        window.addEventListener("keydown", onPressToEscape)
-        function onPressToEscape(event) {
-            if (event.code !== "Escape") {return}
-            if (modalWindow.visible()){
-                modalWindow.close()
-            }
-        }
+       
+        window.addEventListener("keydown", (event) => onPressToEscape(modalWindow))
+}
 
+function onPressToEscape(closedEl) {
+    if (event.code !== "Escape") {return}
+    if (closedEl.visible()){
+        closedEl.close()
+    }
 }
 
 console.log(galleryItems);
+
+
