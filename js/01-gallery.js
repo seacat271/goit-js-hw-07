3bb56a24df44336bb06170861c47d5ead674717a
seacat271 <seacat271083@gmail.com>
2022-11-17 19:12:54 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,22 +7,22 @@ const createGalleryMarkup = galleryItems.map(({preview, original, description}) 
 return `<div class="gallery__item"><a class = "gallery__link" href="${original}"><img class ="gallery__image lazyload" data-src="${preview}" data-source="${original}" alt="${description}" loading="lazy"></a></div>`
 }).join("")
 
-// galleryContainer.innerHTML = createGalleryMarkup;
+galleryContainer.innerHTML = createGalleryMarkup;
 
-// if ("loading" in HTMLImageElement.prototype){
-//     const lazyImages = document.querySelectorAll(".gallery__image")
-//     console.log(lazyImages)
-//     lazyImages.forEach(img => {
-//         img.src = img.dataset.src;
-//     });
-// } else {
-// const scriptLazyLib = document.createElement("script");
-// scriptLazyLib.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
-// scriptLazyLib.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ=="
-// scriptLazyLib.crossOrigin = "anonymous";
-// scriptLazyLib.referrerPolicy = "no-referrer";
-// galleryContainer.insertAdjacentElement("afterend", scriptLazyLib)
-// }
+if ("loading" in HTMLImageElement.prototype){
+    const lazyImages = document.querySelectorAll(".gallery__image")
+    console.log(lazyImages)
+    lazyImages.forEach(img => {
+        img.src = img.dataset.src;
+    });
+} else {
+const scriptLazyLib = document.createElement("script");
+scriptLazyLib.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+scriptLazyLib.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ=="
+scriptLazyLib.crossOrigin = "anonymous";
+scriptLazyLib.referrerPolicy = "no-referrer";
+galleryContainer.insertAdjacentElement("afterend", scriptLazyLib)
+}
 
 galleryContainer.addEventListener("click", onClickGalleryItem);
 
